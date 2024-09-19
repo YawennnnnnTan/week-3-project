5df6f16f868c05257fcaa9e05a7a688aad6ca508
Yawen Tan <yawen.tan@mail.utoronto.ca>
2024-09-20 06:35:56 +0800

    w3

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -9,11 +9,24 @@
 
 
 #### Workspace setup ####
-library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
 # [...ADD CODE HERE...]
+
+library(opendatatoronto)
+library(tidyverse)
+library(dplyr)#inside tidyverse
+set.seed(304)
+start_date<-as.Date('2023-01-01')
+end_date<-as.Date('2024-9-20')
+number_of_date=100
+data<-tibble(date=as.Date(runif(n=number_of_date),min=as.numeric(start_date),max=as.numeric(end_date),origin='1970-01-01'), number_of_marriage=rpois(n=number_of_date,lambda=10))
+#Test data
+data<-read_csv("data/raw_data/simulateed.csv")
+data$number_of_marriage |> min() <=0
+#Test for 
+all(is.na(data$number_of_marriage))
 
 
 
